65bfe34aa218b2203bd8fc94c0705436cf810c74
Srihita Gollapudi <srihitagollapudi@Srihitas-MBP.attlocal.net>
2024-09-03 15:38:16 -0500

    Add content for defualt challenges page

--- challenges/urls.py
+++ challenges/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [  
+urlpatterns = [ 
+    path("", views.list_of_all_months), 
     path("<int:month>",views.monthly_challenges_by_number),
-    path("<str:month>",views.monthly_challenges)
+    path("<str:month>",views.monthly_challenges , name = "month-challenge")
     ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -18,18 +19,30 @@ monthly_challenges_dict = {
 "december": " This is december !!! \n Learn any language for atleast 20min a day!"
 }
 
+def list_of_all_months(request):
+    list_items = ""
+    months = list(monthly_challenges_dict.keys())
+    for each in months:
+        month_path = reverse("month-challenge", args=[each])
+        capitalize_month = each.capitalize()
+        list_items += f"<li><a href =\"{month_path}\">{capitalize_month}</a></li>"
+
+    response_data = f"<ul> {list_items} </ul>"
+    return HttpResponse(response_data)
+
 def monthly_challenges_by_number(request,month):
     forward_month = list(monthly_challenges_dict.keys())
     if month > len(monthly_challenges_dict):
         return(HttpResponseNotFound("Invalid Month!"))
-    
     forward_month = forward_month[month-1]
-    return(HttpResponseRedirect("/challenges/" + forward_month)) 
+    redirect_path = reverse("month-challenge", args=[forward_month])
+    return(HttpResponseRedirect(redirect_path)) 
 
 def monthly_challenges(request,month):
     try:
         return_str = monthly_challenges_dict[str(month)]
-        return(HttpResponse(return_str))
+        response_data = f"<h1>{return_str}</h1>"
+        return(HttpResponse(response_data))
     except:
-        return(HttpResponseNotFound("This month is not supported!"))
+        return(HttpResponseNotFound("<h1>This month is not supported!</h1>"))
 
